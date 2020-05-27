d918fa59bf007c07bb46d3f0d95362c98ba1e882
shishank <shishank@pulseid.com>
2020-05-27 18:36:20 +0530

    NOQA | added book data for fake db

--- main.go
+++ main.go
@@ -6,12 +6,12 @@ import (
 	"time"
 )
 
-var cache = map[int]Book{}
+// var cache = map[int]Book{}
 var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func main() {
-	for i := 0; i < 10; i++ {
-		id := rnd.Intn(10) + 1
-		fmt.Printf("id = %v", id)
+	for i := 0; i < 13; i++ {
+		id := rnd.Intn(13) + 1
+		fmt.Printf("id = %v\n ", id)
 	}
 }
